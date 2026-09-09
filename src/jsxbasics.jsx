8770504235614c15c74cfc266b7fac56396ad083
FatimahHaruna <Fatimaharuna2007@gmail.com>
2026-09-09 10:24:13 +0100

    JSX basics

---
+++ src/jsxbasics.jsx
@@ -0,0 +1,12 @@
+function Name() {
+    const firstName = 'Fatimah';
+    const lastName = 'Haruna';
+    const Age = 19;
+    return (
+        <>
+            <h6>My name is {firstName} {lastName} and I am {Age} years old.</h6>
+        </>
+    )
+}
+
+export default Name;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { createRoot } from "react-dom/client"
 import Hello from "./App"
+import Name from "./jsxbasics"
 import Sport from "./radio"
 
 const root = createRoot(document.getElementById("root"))
 
 root.render(
   <>
+    <Name />
     <Hello />
     <Sport />
   </>
